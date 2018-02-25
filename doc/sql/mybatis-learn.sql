a47616779d9ec025e15ba16d17b632fc611c3322
xooxle <zoozle@qq.com>
2018-02-26 00:31:40 +0800

    update ddl

--- doc/sql/mybatis-learn.sql
+++ doc/sql/mybatis-learn.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2018-02-25 23:40:08
+Date: 2018-02-26 00:30:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,13 +20,14 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
-  `id` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', '寮犱笁', 'zhangsan@qq.com');
+INSERT INTO `sys_user` VALUES ('1', '张三', 'zhangsan@qq.com');
+INSERT INTO `sys_user` VALUES ('2', '张三丰', 'zhangsanfeng@qq.com');
